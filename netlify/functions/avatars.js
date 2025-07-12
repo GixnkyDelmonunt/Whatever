868138897a84af49ff94a0e7cdbb976049a960fa
GixnkyDelmonunt <131413385+GixnkyDelmonunt@users.noreply.github.com>
2025-07-12 14:02:34 -0500

    Update avatars.js

--- netlify/functions/avatars.js
+++ netlify/functions/avatars.js
@@ -8,7 +8,7 @@ exports.handler = async function(event) {
   }
 
   try {
-    const response = await fetch(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${userIds}&size=420x420&format=Png&isCircular=false`);
+    const response = await fetch(`https://thumbnails.roblox.com/v1/users/avatar?userIds=${userIds}&size=420x420&format=Png&isCircular=false`);
     const data = await response.json();
 
     return {
